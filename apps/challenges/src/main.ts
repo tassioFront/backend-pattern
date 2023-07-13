da087f32d3e22d6b400b40a02fcc0817d045b17c
Tássio <tassio72@hotmail.com>
2023-07-13 18:06:52 -0300

    chore: add headers

--- apps/challenges/src/main.ts
+++ apps/challenges/src/main.ts
@@ -6,6 +6,13 @@ import challengeRouter from './challenges.router';
 
 app.use(challengeRouter);
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   process.env.NODE_ENV !== 'production' &&
