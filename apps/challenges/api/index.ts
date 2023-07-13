44057dd7ec82e6eb1ad12e62e8348e3bb3f2cc03
Tássio <tassio72@hotmail.com>
2023-07-13 18:43:35 -0300

    chore: add api folder

---
+++ apps/challenges/api/index.ts
@@ -0,0 +1,23 @@
+import * as express from 'express';
+
+const app = express();
+
+import challengeRouter from '../src/challenges.router';
+
+app.use(challengeRouter);
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// const port = process.env.port || 3333;
+// const server = app.listen(port, () => {
+//   process.env.NODE_ENV !== 'production' &&
+//     console.log(`Listening at http://localhost:${port}`);
+// });
+// server.on('error', console.error);
+
+export default app;
