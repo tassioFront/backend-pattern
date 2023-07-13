a54dc1a7877447b370cd59710130dd3c90623dae
Tássio <tassio72@hotmail.com>
2023-07-13 18:19:54 -0300

    chore: try another config

--- apps/challenges/src/index.ts
+++ apps/challenges/src/index.ts
@@ -13,9 +13,11 @@ app.use((req, res, next) => {
   next();
 });
 
-const port = process.env.port || 3333;
-const server = app.listen(port, () => {
-  process.env.NODE_ENV !== 'production' &&
-    console.log(`Listening at http://localhost:${port}`);
-});
-server.on('error', console.error);
+// const port = process.env.port || 3333;
+// const server = app.listen(port, () => {
+//   process.env.NODE_ENV !== 'production' &&
+//     console.log(`Listening at http://localhost:${port}`);
+// });
+// server.on('error', console.error);
+
+module.exports = app;
