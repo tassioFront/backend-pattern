3e28a691437609499cd3f62bdb0fbed520c2e4c8
Tássio <tassio72@hotmail.com>
2023-07-13 18:51:38 -0300

    chore: add api folder with js

--- apps/challenges/api/index.js
+++ apps/challenges/api/index.js
@@ -1,11 +1,9 @@
 const app = require('express')();
-const { v4 } = require('uuid');
 
 app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+  res.send(`Hello! Go to item`);
 });
 
 app.get('/api/item/:slug', (req, res) => {
