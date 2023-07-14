2126b782fe2668543a98cc59583ae4e86ba72d1b
Tássio <tassio72@hotmail.com>
2023-07-14 08:52:36 -0300

    chore: get back old config

--- apps/challenges/api/teste.js
+++
@@ -1,6 +0,0 @@
-export default function handler(request, response) {
-  // api/[name].js -> /api/lee
-  // request.query.name -> "lee"
-  const { name } = request.query;
-  return response.end(`Hello ${name}!`);
-}
